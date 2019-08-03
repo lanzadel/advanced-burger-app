f3bd1dc9575360806c47a126f747980afd42ff11
mchakara <mohamed.chakara@capgemini.com>
2019-08-04 00:10:57 +0100

    12. Adding Routing to our Burger Project

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,10 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
+import {withRouter}  from 'react-router-dom';
+
 const burger = (props) => {
+    console.log(props)
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -28,4 +31,4 @@ const burger = (props) => {
     );
 }
 
-export default burger;
+export default withRouter(burger);
